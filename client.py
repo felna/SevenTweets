681952f447e09d681928ba193c2b8132e20e1fc7
Nikola Novkovic <itsdaddy2001@gmail.com>
2017-04-16 17:12:19 +0200

    First commit

---
+++ client.py
@@ -0,0 +1,14 @@
+import requests, json
+
+#resp = requests.get("http://127.0.0.1:5000/hello/Nikola")
+
+poster = requests.post(
+    url="http://127.0.0.1:5000/poster/",
+    params={'my-param-name': 'param-value'},
+    headers={'my-header-name': 'header-value'},
+    json=json.dumps({'some': 'value'})
+)
+print(poster.status_code)
+print(poster.text)
+print(poster.reason)
+print(poster.links)
---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world!"
+
+@app.route("/poster/", methods=["POST"])
+def poster():
+    print(request.json)
+    print(request.headers)
+    return "Hello world!"
+
+@app.route("/hello/<username>")
+def hello_name(username):
+    return "Hello " + username + "!"
+
+if __name__ == "__main__":
+    app.run()
