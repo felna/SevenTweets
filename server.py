5ca737d5141cef488a8ab34dd6dc1e2cd01ee701
Nikola Novkovic <itsdaddy2001@gmail.com>
2017-04-18 11:16:06 +0200

    Copied default Storage class | Modified empty lines (PEP8)

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello world!"
+
 
 @app.route("/poster/", methods=["POST"])
 def poster():
@@ -12,9 +14,11 @@ def poster():
     print(request.headers)
     return "Hello world!"
 
+
 @app.route("/hello/<username>")
 def hello_name(username):
     return "Hello " + username + "!"
+
 
 if __name__ == "__main__":
     app.run()
